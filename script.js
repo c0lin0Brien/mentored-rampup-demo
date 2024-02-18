87e61923162d84bf6b025a675625092d3c652ff0
ColinO'Brien <colinpobrien04@gmail.com>
2024-02-17 16:41:14 -0800

    Added first instance of audio functionality

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let playing = false;
+let audio = null;
+
 const sonicYouth = document.getElementById('sonicyouth');
 sonicYouth.addEventListener('click', function() {
     console.log("SonicYouth!");
+    if (!playing) {
+        audio = new Audio('Songs/KoolThing.wav');
+        audio.play();
+        playing = true;
+        setTimeout(queue, 10000);
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+        console.log("paused");
+    }
 })
+
+function queue () {
+    playing = false;
+}
